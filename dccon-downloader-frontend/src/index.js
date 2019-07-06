603f7c9e209e9c38170252eaff569493f2c11f6d
Sangyun Pae <bsy@breadlab.net>
2019-07-06 22:58:12 +0900

    Added react-ga

--- dccon-downloader-frontend/src/index.js
+++ dccon-downloader-frontend/src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import ReactGA from 'react-ga';
+
+ReactGA.initialize('UA-143376494-1');
+ReactGA.pageview('/');
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
